ee0c7069407fee414490048dad62b2afc1755243
zhanghuan <aystzh@163.com>
2020-03-18 15:19:24 +0800

    gin hello word

---
+++ gin/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	res := gin.Default() //使用了gin.Default()生成了一个实例，这个实例即 WSGI 应用程序。
+	res.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello,Gin!")
+	}) //声明一个路由
+	res.Run(":3333") //监听9999端口并启动本地服务 注意前面必须加 :
+}
